cd133aeeb24c3a5480ceeeb326f93a5fe0e0ed08
Misha <pavlich1971@gmail.com>
2023-07-13 22:26:47 +0300

    add fun dov

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hellow page git");
+function dov(){
+    var lop = [0,1,2,3,4,5];
+    for (let index = 0; index < array.length; index++) {
+        const element = lop[index];
+        console.log(element);
+    }
+}
+window.onload = function(){
+    dov();
+}
