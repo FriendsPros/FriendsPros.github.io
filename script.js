d10bcb41e28bb8ad01e8f8e57d34785e69922e36
Misha <pavlich1971@gmail.com>
2023-07-13 22:43:31 +0300

    eroor UPDATE script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("hellow page git");
 function dov(){
     var lop = [0,1,2,3,4,5];
-    for (let index = 0; index < array.length; index++) {
+    for (let index = 0; index < lop.length; index++) {
         const element = lop[index];
         console.log(element);
     }
