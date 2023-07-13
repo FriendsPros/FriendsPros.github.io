bceecfa724ad83522923b43a27abe7b441b8b2d5
Misha <pavlich1971@gmail.com>
2023-07-13 22:53:08 +0300

    robota

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ function dov(){
     for (let index = 0; index < lop.length; index++) {
         const element = lop[index];
         console.log(element);
-    }
+        addtimer(index);
+        setTimeout(function(){
+            if(index == lop.length){
+            dov();
+        }
+    },1000);
+
+}
+}
+function addtimer(params) {
+    document.querySelectorAll("#status").text = params;
 }
 window.onload = function(){
     dov();
